30f3558c574c9f5699350e8f79d179a240d73ff2
Dexter Zhang <dejzhang@ucsc.edu>
2023-06-04 21:14:08 -0700

    Dexter Zhang added the scenes

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
-class Intro extends Phaser.Scene
+class Title extends Phaser.Scene
 {   
     constructor() {
-        super('intro')
+        super('title')
     }
-    
     create ()
     {
         
 
-        const text = this.add.text(400, 300, 'Path of Purity\nCinematics', { align: 'center' }, 0xFF69B4);
+        const text = this.add.text(400, 300, 'Title Scene', { align: 'center' }, 0xFF69B4);
         text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
 
         text.setOrigin(0.5, 0.5);
@@ -16,38 +15,196 @@ class Intro extends Phaser.Scene
         text.setFontFamily('Arial');
         text.setFontStyle('bold');
         text.setFontSize(100);
-
         text.preFX.setPadding(32);
-
         const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
 
-        // adding start button 
-
-        
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('victory'));
+            this.time.delayedCall(1000, () => this.scene.start('transition1'));
+        });
+    }
+}
+class Transition1 extends Phaser.Scene
+{   
+    constructor() {
+        super('transition1')
+    }
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'Transition 1', { align: 'center' }, 0xFF69B4);
+        text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
+        text.setOrigin(0.5, 0.5);
+        text.setResolution(window.devicePixelRatio);
+        text.setFontFamily('Arial');
+        text.setFontStyle('bold');
+        text.setFontSize(100);
+        text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('gameplay'));
+        });
+    }
+}
+class Gameplay extends Phaser.Scene
+{   
+    constructor() {
+        super('gameplay')
+    }
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'Gameplay', { align: 'center' }, 0xFF69B4);
+        text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
+        text.setOrigin(0.5, 0.5);
+        text.setResolution(window.devicePixelRatio);
+        text.setFontFamily('Arial');
+        text.setFontStyle('bold');
+        text.setFontSize(100);
+        text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('transition2'));
+        });
+    }
+}
+class Transition2 extends Phaser.Scene
+{   
+    constructor() {
+        super('transition2')
+    }
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'Transition 2', { align: 'center' }, 0xFF69B4);
+        text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
+        text.setOrigin(0.5, 0.5);
+        text.setResolution(window.devicePixelRatio);
+        text.setFontFamily('Arial');
+        text.setFontStyle('bold');
+        text.setFontSize(100);
+        text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('transition3'));
+        });
+    }
+}
+class Transition3 extends Phaser.Scene
+{   
+    constructor() {
+        super('transition3')
+    }
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'Transition 3', { align: 'center' }, 0xFF69B4);
+        text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
+        text.setOrigin(0.5, 0.5);
+        text.setResolution(window.devicePixelRatio);
+        text.setFontFamily('Arial');
+        text.setFontStyle('bold');
+        text.setFontSize(100);
+        text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('stat'));
+        });
+    }
+}
+class Stat extends Phaser.Scene
+{   
+    constructor() {
+        super('stat')
+    }
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'Stat Scene', { align: 'center' }, 0xFF69B4);
+        text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
+        text.setOrigin(0.5, 0.5);
+        text.setResolution(window.devicePixelRatio);
+        text.setFontFamily('Arial');
+        text.setFontStyle('bold');
+        text.setFontSize(100);
+        text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('end'));
         });
     }
 }
 
-class Victory extends Phaser.Scene{
+class End extends Phaser.Scene
+{   
     constructor() {
-        super('victory');
+        super('end')
     }
-    
-    create() {
-        const text = this.add.text(400, 150, 'Congrats you win!', { align: 'center' }, 0xFF69B4);
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'End Screen', { align: 'center' }, 0xFF69B4);
         text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
         text.setOrigin(0.5, 0.5);
         text.setResolution(window.devicePixelRatio);
         text.setFontFamily('Arial');
         text.setFontStyle('bold');
         text.setFontSize(100);
-
         text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
 
-        this.input.on('pointerdown', () => this.scene.start('intro'));
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('credits'));
+        });
+    }
+}
+class Credits extends Phaser.Scene
+{   
+    constructor() {
+        super('credits')
+    }
+    create ()
+    {
+        
+
+        const text = this.add.text(400, 300, 'Credit Scene', { align: 'center' }, 0xFF69B4);
+        text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+
+        text.setOrigin(0.5, 0.5);
+        text.setResolution(window.devicePixelRatio);
+        text.setFontFamily('Arial');
+        text.setFontStyle('bold');
+        text.setFontSize(100);
+        text.preFX.setPadding(32);
+        const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('title'));
+        });
     }
 }
 
@@ -57,7 +214,7 @@ const config = {
     height: 600,
     backgroundColor: '#FFC0CB',
     parent: 'phaser-example',
-    scene: [Intro, Victory]
+    scene: [Title, Transition1, Gameplay, Transition2, Transition3, Stat, End, Credits]
 };
 
 const game = new Phaser.Game(config);
