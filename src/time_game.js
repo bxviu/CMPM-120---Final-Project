0565076c2fe15b0bb3c3cdc9c07db2e8aff88ad2
Benthan Vu <bxvu@ucsc.edu>
2023-06-09 20:55:15 -0700

    changed movement to joystick, added dexter's tilemap, zoomed in camera

--- src/time_game.js
+++ src/time_game.js
@@ -5,8 +5,11 @@ class Intro extends Phaser.Scene
     }
 
     preload() {
+        this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
+
         this.load.path = './data/';
-        this.load.tilemapTiledJSON("map", "rpgtilemap.tmj");
+        this.load.tilemapTiledJSON("map", "Getting_Old_Tilemap_large_v1.1.1.json");
+        // this.load.tilemapTiledJSON("map", "rpgtilemap.tmj");
 
         this.load.path = './assets/tilemap/';
         this.load.image("tiles", "First Asset pack.png");
@@ -61,7 +64,7 @@ class Intro extends Phaser.Scene
         let bgMusic = this.sound.add('bgMusic', { loop: true });
         bgMusic.play();
         console.log(this)
-        this.player = (new Player(this, 400, 300, 'InputName', {speed:300, items:[]}));
+        this.player = (new Player(this, 20, 145, 'InputName', {speed:100, items:[]}));
 
         // console.log(Phaser.GameObjects.Sprite)
 
@@ -72,32 +75,95 @@ class Intro extends Phaser.Scene
         // Phaser's cache (i.e. the name you used in preload)
         const tileset = map.addTilesetImage("rpgtileset", "tiles");
 
+            // tileset.setInteractive({useHandCursor:true});
+        // var drag = this.plugins.get('rexdragplugin').add();
+        
+
         // Parameters: layer name (or index) from Tiled, tileset, x, y
-        const belowLayer = map.createLayer("below", tileset, 0, 0);
-        const worldLayer = map.createLayer("world", tileset, 0, 0);
-        const aboveLayer = map.createLayer("above", tileset, 0, 0);
-        aboveLayer.setDepth(10);
+        // const belowLayer = map.createLayer("below", tileset, 0, 0);
+        // const worldLayer = map.createLayer("world", tileset, 0, 0);
+        // const aboveLayer = map.createLayer("above", tileset, 0, 0);
+        // aboveLayer.setDepth(10);
+        // console.log( tileset.getTileLayerNames());
+        const ground = map.createLayer("Ground", tileset, 0, 0);
+        const cliffs = map.createLayer("Cliffs", tileset, 0, 0);
+        const cliffs2 = map.createLayer("Cliffs 2", tileset, 0, 0);
+        const plants1 = map.createLayer("Plants", tileset, 0, 0);
+        const plants2 = map.createLayer("Plants 2", tileset, 0, 0);
+        const plants3 = map.createLayer("Plants 3", tileset, 0, 0);
+        const plants4 = map.createLayer("Plants 4", tileset, 0, 0);
+        const fences = map.createLayer("Fences", tileset, 0, 0);
+        const buildings = map.createLayer("Buildings", tileset, 0, 0);
+        const collision = map.createLayer("Collision", tileset, 0, 0);
 
-        worldLayer.setCollisionByProperty({ collides: true });
+        collision.setCollisionBetween(1, 999, true, 'Collision');
 
-        // const debugGraphics = this.add.graphics().setAlpha(0.75);
-        // worldLayer.renderDebug(debugGraphics, {
-        //     tileColor: null, // Color of non-colliding tiles
-        //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-        //     faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-        //     });
+        // worldLayer.setCollisionByProperty({ collides: true });
+        // worldLayer.setCollisionBetween(1, 999, true, 'world');
+        const debugGraphics = this.add.graphics().setAlpha(0.75);
+        collision.renderDebug(debugGraphics, {
+            tileColor: null, // Color of non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+            });
+
+        collision.forEachTile(function(tile, index, tileArray) {
+            tile.setAlpha(0);
+        })
             
-        this.physics.add.collider(this.player, worldLayer);
+        this.physics.add.collider(this.player, collision);
+        this.makeUI();
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
         this.camFocusX = this.player.x;
         this.camFocusY = this.player.y;
-        // this.cameras.main.startFollow(this.player);
-        // this.cameras.main.setZoom();
-        // this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.camFocusPlayer = false;
+        console.log(this.cameras.main)
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setZoom(3);
+        this.cameras.main.setLerp(0.1, 0.1);
 
-        // this.items = [];
-        // this.items.push(new Item(this, 20, 400, 'bow', 'item1', {displayName:'Placeholder-Bow'}));
-        // this.items.push(new Item(this, 300, 600, 'arrow', 'item2', {displayName:'Placeholder-Arrow'}));
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.onJoyStick = false;
+        this.joyStick.on('pointerdown', (pointer) => {this.onJoyStick = true;})
+        this.joyStick.on('pointerup', (pointer) => {this.onJoyStick = false;});
+
+        this.initialPointerPosition = null;
+        this.input.on('pointerdown', function(pointer) {
+            console.log(this.onJoyStick)
+            // this.time.delayedCall(500, () => {
+                if (!this.onJoyStick) {
+                // if (!(this.moving.left || this.moving.right || this.moving.up || this.moving.down)){
+                    this.camFocusPlayer = false;
+                    // Store the initial pointer position
+                    this.initialPointerPosition = new Phaser.Math.Vector2(pointer.x, pointer.y);
+                    // console.log(this.initialPointerPosition)
+                    this.cameras.main.stopFollow();
+                }
+            // });
+        }, this);
+    
+        this.input.on('pointermove', function(pointer) {
+            if (this.initialPointerPosition) {
+                // Calculate the distance moved by the pointer
+                var deltaX = pointer.x - this.initialPointerPosition.x;
+                var deltaY = pointer.y - this.initialPointerPosition.y;
+    
+                // Move the camera accordingly
+                this.cameras.main.scrollX -= deltaX;
+                this.cameras.main.scrollY -= deltaY;
+                this.camFocusX = this.cameras.main.midPoint.x;
+                this.camFocusY = this.cameras.main.midPoint.y;
+    
+                // Update the initial pointer position
+                this.initialPointerPosition.set(pointer.x, pointer.y);
+            }
+        }, this);
+    
+        this.input.on('pointerup', function(pointer) {
+            // Reset the initial pointer position when the dragging ends
+            this.initialPointerPosition = null;
+        }, this);
 
         this.sceneDuration = 0;
         this.timer = 0;
@@ -111,66 +177,69 @@ class Intro extends Phaser.Scene
             up: false,
             down: false,
         };
-        this.makeUI();
 
         //move player left when clicked
-        this.leftArrow.setInteractive({useHandCursor: true});
-        this.leftArrow.on('pointerdown', () => {
-            this.moving.left = true;
-            this.player.play("sideways");
-        })
-        .on('pointerout', () => {
-            this.moving.left = false;
-            this.player.stopAnim();
-        })
-        .on('pointerup', () => {
-            this.moving.left = false;
-            this.player.stopAnim();
-        });
-        this.rightArrow.setInteractive({useHandCursor: true});
-        this.rightArrow.on('pointerdown', () => {
-            this.moving.right = true;
-            this.player.play("sideways");
-        })
-        .on('pointerout', () => {
-            this.moving.right = false;
-            this.player.stopAnim();
-        })
-        .on('pointerup', () => {
-            this.moving.right = false;
-            this.player.stopAnim();
-        });
-        this.upArrow.setInteractive({useHandCursor: true});
-        this.upArrow.on('pointerdown', () => {
-            this.moving.up = true;
-            this.player.play("up");
+        // this.leftArrow.setInteractive({useHandCursor: true});
+        // this.leftArrow.on('pointerdown', () => {
+        //     this.moving.left = true;
+        //     this.camFocusPlayer = true;
+        //     this.player.play("sideways");
+        // })
+        // .on('pointerout', () => {
+        //     this.moving.left = false;
+        //     this.player.stopAnim();
+        // })
+        // .on('pointerup', () => {
+        //     this.moving.left = false;
+        //     this.player.stopAnim();
+        // });
+        // this.rightArrow.setInteractive({useHandCursor: true});
+        // this.rightArrow.on('pointerdown', () => {
+        //     this.moving.right = true;
+        //     this.camFocusPlayer = true;
+        //     this.player.play("sideways");
+        // })
+        // .on('pointerout', () => {
+        //     this.moving.right = false;
+        //     this.player.stopAnim();
+        // })
+        // .on('pointerup', () => {
+        //     this.moving.right = false;
+        //     this.player.stopAnim();
+        // });
+        // this.upArrow.setInteractive({useHandCursor: true});
+        // this.upArrow.on('pointerdown', () => {
+        //     this.moving.up = true;
+        //     this.camFocusPlayer = true;
+        //     this.player.play("up");
 
-        })
-        .on('pointerout', () => {
-            this.moving.up = false;
-            this.player.stopAnim();
-        })
-        .on('pointerup', () => {
-            this.moving.up = false;
-            this.player.stopAnim();
-        });
-        this.downArrow.setInteractive({useHandCursor: true});
-        this.downArrow.on('pointerdown', () => {
-            this.moving.down = true;
-            this.player.play("down");
+        // })
+        // .on('pointerout', () => {
+        //     this.moving.up = false;
+        //     this.player.stopAnim();
+        // })
+        // .on('pointerup', () => {
+        //     this.moving.up = false;
+        //     this.player.stopAnim();
+        // });
+        // this.downArrow.setInteractive({useHandCursor: true});
+        // this.downArrow.on('pointerdown', () => {
+        //     this.moving.down = true;
+        //     this.camFocusPlayer = true;
+        //     this.player.play("down");
 
-        })
-        .on('pointerout', () => {
-            this.moving.down = false;
-            this.player.stopAnim();
-        })
-        .on('pointerup', () => {
-            this.moving.down = false;
-            this.player.stopAnim();
-        });
+        // })
+        // .on('pointerout', () => {
+        //     this.moving.down = false;
+        //     this.player.stopAnim();
+        // })
+        // .on('pointerup', () => {
+        //     this.moving.down = false;
+        //     this.player.stopAnim();
+        // });
 
-    }    
-
+    }
+    
     update(time, delta) {
         this.delta = delta;
         this.sceneDuration = this.sys.game.loop.time - this.startTime;
@@ -178,34 +247,73 @@ class Intro extends Phaser.Scene
         // Stop any previous movement from the last frame
         this.player.body.velocity.x = 0;
         this.player.body.velocity.y = 0;
-        this.cameras.main.centerOn(this.camFocusX, this.camFocusY);
-      
+
+        if (this.joyStick.left) {
+            this.camFocusPlayer = true;
+            this.moving.left = true;
+        }
+        else if (this.joyStick.right) {
+            this.camFocusPlayer = true;
+            this.moving.right = true;
+        }
+        else {
+            this.moving.left = false;
+            this.moving.right = false;
+            if (this.player.anims.getName() == 'sideways')
+                this.player.stopAnim();
+        }
+
+        if (this.joyStick.up) {
+            this.camFocusPlayer = true;
+            this.moving.up = true;
+        }
+        else if (this.joyStick.down) {
+            this.camFocusPlayer = true;
+            this.moving.down = true;
+        }
+        else {
+            this.moving.up = false;
+            this.moving.down = false;
+            if (this.player.anims.getName() == 'up' || this.player.anims.getName() == 'down')
+                this.player.stopAnim();
+        }
+
+        if (this.joyStick.noKey) {
+            this.moving.left = false;
+            this.moving.right = false;
+            this.moving.up = false;
+            this.moving.down = false;
+            this.player.stopAnim();
+        }
+
         // Horizontal movement
         if (this.moving.left) {
             this.player.flipX = true;
+            if (!this.player.anims.isPlaying) 
+                this.player.play("sideways");
             this.player.body.velocity.x = -this.player.speed;
-            if (this.camFocusX > this.player.x - this.player.speed/2)
-                this.camFocusX = this.camFocusX - delta * this.player.speed/667;
         } else if (this.moving.right) {
             this.player.flipX = false;
+            if (!this.player.anims.isPlaying) 
+                this.player.play("sideways");
             this.player.body.velocity.x = this.player.speed;
-            if (this.camFocusX < this.player.x + this.player.speed/2)
-                this.camFocusX = this.camFocusX + delta * this.player.speed/667;
         }
       
         // Vertical movement
         if (this.moving.up) {
             this.player.body.velocity.y = -this.player.speed;
-            if (this.camFocusY > this.player.y - this.player.speed/2)
-                this.camFocusY = this.camFocusY - delta * this.player.speed/667;
+            if (!this.player.anims.isPlaying) 
+                this.player.play("up");
         } else if (this.moving.down) {
             this.player.body.velocity.y = this.player.speed;
-            if (this.camFocusY < this.player.y + this.player.speed/2) {
-                this.camFocusY = this.camFocusY + delta * this.player.speed/667;
-            }
+            if (!this.player.anims.isPlaying) 
+                this.player.play("down");
         }
 
-        if (!(this.moving.left || this.moving.right || this.moving.up || this.moving.down)) {
+        let reachedPlayerX = false;
+        let reachedPlayerY = false;
+        if (this.camFocusPlayer) {
+            this.cameras.main.centerOn(this.camFocusX, this.camFocusY);
             if (this.camFocusX > this.player.x+this.player.speed/20) {
                 this.camFocusX = this.camFocusX - delta * this.player.speed/333;
             }
@@ -214,6 +322,7 @@ class Intro extends Phaser.Scene
             }
             else {
                 this.camFocusX = this.player.x;
+                reachedPlayerX = true;
             }
             if (this.camFocusY > this.player.y+this.player.speed/20) {
                 this.camFocusY = this.camFocusY - delta * this.player.speed/333;
@@ -223,8 +332,12 @@ class Intro extends Phaser.Scene
             }
             else {
                 this.camFocusY = this.player.y;
+                reachedPlayerY = true;
             }
-            // this.camAnimOffset = Math.max(this.camAnimOffset - delta * 0.1, 0)
+            if (reachedPlayerX && reachedPlayerY) {
+                this.cameras.main.startFollow(this.player);
+                this.cameras.main.setLerp(0.1, 0.1);
+            }
         }
       
     }
@@ -237,12 +350,41 @@ class Intro extends Phaser.Scene
 
         this.add.text(400, this.player.y-50, "arrows to move, \ninteract while touching an item to pick it up, \ntap inventory to open inventory")
 
-        this.leftArrow = this.add.image(50, height-100, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5).setRotation(-Math.PI/2);
-        this.rightArrow = this.add.image(150, height-100, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5).setRotation(Math.PI/2);
-        this.upArrow = this.add.image(100, height-150, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5);
-        this.downArrow = this.add.image(100, height-50, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5).setRotation(Math.PI);
-        this.inventoryButton = this.add.image(100, 75, "inventoryButton").setScrollFactor(0).setScale(2).setOrigin(0.5);
-        this.interactButton = this.add.image(width-90, height-100, "interactButton").setScrollFactor(0).setScale(2).setOrigin(0.5);
+        // this.leftJoystick = this.rexUI.add.virtualJoystick({
+        //     x: 50,
+        //     y: height-100,
+        //     radius: 50,
+        //     base: this.add.circle(0, 0, 50, 0x888888).setAlpha(0.5),
+        //     thumb: this.add.circle(0, 0, 25, 0x888888),
+        //     dir: '8dir',
+        //     input: this.input
+        // })
+        this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+            x: 315,
+            y: 350,
+            radius: 50,
+            base: this.add.circle(0, 0, 50, 0x888888).setAlpha(0.5),
+            thumb: this.add.circle(0, 0, 25, 0x888888),
+            dir: '8dir',
+            // forceMin: 16,
+            // fixed: true,
+            // enable: true
+        }); 
+
+        this.leftArrow = this.add.image(290, 350, "movementArrow").setScrollFactor(0).setScale(0.5).setOrigin(0.5).setRotation(-Math.PI/2).setAlpha(0.5);
+        this.rightArrow = this.add.image(340, 350, "movementArrow").setScrollFactor(0).setScale(0.5).setOrigin(0.5).setRotation(Math.PI/2).setAlpha(0.5);
+        this.upArrow = this.add.image(315, 325, "movementArrow").setScrollFactor(0).setScale(0.5).setOrigin(0.5).setAlpha(0.5);
+        this.downArrow = this.add.image(315, 375, "movementArrow").setScrollFactor(0).setScale(0.5).setOrigin(0.5).setRotation(Math.PI).setAlpha(0.5);
+        this.inventoryButton = this.add.image(315, 240, "inventoryButton").setScrollFactor(0).setScale(0.75).setOrigin(0.5).setAlpha(0.5);
+        this.interactButton = this.add.image(490, 350, "interactButton").setScrollFactor(0).setScale(0.75).setOrigin(0.5).setAlpha(0.5);
+
+        // when zoom is 1 (probably wont happen anymore)
+        // this.leftArrow = this.add.image(50, height-100, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5).setRotation(-Math.PI/2);
+        // this.rightArrow = this.add.image(150, height-100, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5).setRotation(Math.PI/2);
+        // this.upArrow = this.add.image(100, height-150, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5);
+        // this.downArrow = this.add.image(100, height-50, "movementArrow").setScrollFactor(0).setScale(1).setOrigin(0.5).setRotation(Math.PI);
+        // this.inventoryButton = this.add.image(100, 75, "inventoryButton").setScrollFactor(0).setScale(2).setOrigin(0.5);
+        // this.interactButton = this.add.image(width-90, height-100, "interactButton").setScrollFactor(0).setScale(2).setOrigin(0.5);
 
         let plinkNoise = this.sound.add('plink', { loop: false });
 
@@ -286,6 +428,7 @@ class Entity extends Phaser.GameObjects.Sprite
         this.imageKey = image;
         this.displayName = config.displayName || name;
         this.scene.physics.add.existing(this);
+        this.body.setCollideWorldBounds(true);
     }
 
 }
@@ -297,6 +440,8 @@ class Player extends Entity
         this.createAnimations();
         this.items = config.items || [];
         this.speed = config.speed || 100;
+        this.body.setSize(this.width-5, this.height-10);
+        this.body.setOffset(2.5, 10);
     }
 
     createAnimations() {
@@ -374,7 +519,7 @@ const config = {
         default: "arcade",
         arcade: {
           gravity: { y: 0 } // Top down game, so no gravity
-        }
+        },
     },
     scene: [Intro, Inventory]
 };
