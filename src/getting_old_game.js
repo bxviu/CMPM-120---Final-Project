18c54bc9620c6f472f1109908ecb7c7fce96fdce
Dexter Zhang <dejzhang@ucsc.edu>
2023-06-13 14:28:54 -0700

    Dexter Zhang update scenes

--- src/getting_old_game.js
+++ src/getting_old_game.js
@@ -11,23 +11,57 @@
         {
             this.data = Object.keys(data).length === 0 ? {limit: 60} : data;
         }
+        idleMotionTween(object, x, y){
+            this.tweens.chain({
+                targets: object,
+                repeat: -1,
+                tweens:[
+                {x: x+Phaser.Math.Between(0,4),y:y+Phaser.Math.Between(-4,4),ease:"sine.inout",duration:3000, delay:Phaser.Math.Between(0,1000), yoyo: true},
+                {x: x-Phaser.Math.Between(0,4),y:y+Phaser.Math.Between(-4,4),ease:"sine.inout",duration:3000, delay:Phaser.Math.Between(0,1000), yoyo:true},
+            ]});
+        }
+        hoverInteract(text){
+            text.on("pointerover",()=>{
+                text.setColor("#ffffff");
+                console.log("deez");
+            })
+            text.on("pointerout",()=>{
+                text.setColor("#EDC02C");
+                console.log("nuts");
+            })
+        }
         create ()
         {
             
-            const text = this.add.text(400, 300, 'Title Scene \n-dex was here', { align: 'center' }, 0xFF69B4);
-            text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+            this.add.rectangle(0, 0, 2000, 2000, 0xA2EEEB);
+            // example objects
+            let recPosX = Phaser.Math.Between(100,700);
+            let recPosY = Phaser.Math.Between(100,700);
+            this.screensaver= this.add.rectangle(recPosX, recPosY, 200, 200, 0x000000);
+            this.physics.add.existing(this.screensaver);
+            this.screensaver.body.setGravity(0, 0).setBounce(1).setCollideWorldBounds(true).setVelocity(200);
+            let circlePosX = Phaser.Math.Between(100,700);
+            let circlePosY = Phaser.Math.Between(100,300);
+            this.ballin=this.add.circle(circlePosX,circlePosY, 100, 0xffffff);
+            this.physics.add.existing(this.ballin);
+            this.ballin.body.setCircle(100).setGravity(0, 100).setBounce(1).setCollideWorldBounds(true).setVelocity(-200, -100);
+            // text
+            const logo = this.add.text(400, 100, "Getting Old", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:100,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5);
+            const start = this.add.text(400, 250, "Begin living", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:30,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5).setInteractive();
+            const inventory = this.add.text(400, 300, "View your memories", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:30,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5).setInteractive();
+            const credits = this.add.text(400, 350, "Credits", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:30,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5).setInteractive();
 
-            text.setOrigin(0.5, 0.5);
-            text.setResolution(window.devicePixelRatio);
-            text.setFontFamily('Arial');
-            text.setFontStyle('bold');
-            text.setFontSize(100);
-            text.preFX.setPadding(32);
-            const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+            this.idleMotionTween(logo, 400, 100);
+            this.idleMotionTween(start, 400, 250);
+            this.idleMotionTween(inventory, 400, 300);
+            this.idleMotionTween(credits, 400, 350);
 
-            this.input.on('pointerdown', () => {
-                this.cameras.main.fade(1000, 0,0,0);
-                this.time.delayedCall(1000, () => this.scene.start('transition1', this.data));
+            this.hoverInteract(start);
+            this.hoverInteract(inventory);
+            this.hoverInteract(credits);
+
+            start.on("pointerdown", ()=>{
+                this.time.delayedCall(1000, () => this.scene.start('transition1'));
             });
         }
     }
@@ -40,25 +74,64 @@
         {
             this.data = Object.keys(data).length === 0 ? {limit: 90} : data;
         }
+        fadeInTween(object){
+            this.tweens.add({
+                targets: object,
+                alpha: { from: 0, to: 1 },
+                ease: 'Sine.InOut',
+                duration: 1000,
+            });
+        }
+        hoverInteract(text){
+            text.on("pointerover",()=>{
+                text.setColor("#ffffff");
+                console.log("deez");
+            })
+            text.on("pointerout",()=>{
+                text.setColor("#EDC02C");
+                console.log("nuts");
+            })
+        }
+        
         create ()
         {
-            
-
-            const text = this.add.text(400, 300, 'Transition 1', { align: 'center' }, 0xFF69B4);
-            text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
-
-            text.setOrigin(0.5, 0.5);
-            text.setResolution(window.devicePixelRatio);
-            text.setFontFamily('Arial');
-            text.setFontStyle('bold');
-            text.setFontSize(100);
-            text.preFX.setPadding(32);
-            const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
-
-            this.input.on('pointerdown', () => {
-                this.cameras.main.fade(1000, 0,0,0);
-                this.time.delayedCall(1000, () => this.scene.start('gameplay', this.data));
+            this.counter = 0;
+            this.add.rectangle(0, 0, 2000, 2000, 0xA2EEEB);
+            const par1 = this.add.text(400, 150, "You've come home from a busy day at school.\nYour parents are proud\nYou've finished your second year of school.", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:30,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5);
+            const par2 = this.add.text(400, 300, "Now here you stand at home,\nTwo months of summer break!\nWith a long time to explore limitless possibilities.", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:30,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5);
+            const par3 = this.add.text(400, 400, "sample button here", {align: "center",fontFamily:"Baskerville",fontStyle:"bold",fontSize:30,color:"#EDC02C",resolution:window.devicePixelRatio,}).setOrigin(0.5,0.5);
+            par1.setAlpha(0);
+            par2.setAlpha(0);
+            par3.setAlpha(0);
+            par3.setInteractive();
+            this.tweens.add({
+                targets: par1,
+                alpha: { from: 0, to: 1 },
+                ease: 'Sine.InOut',
+                duration: 1000,
             });
+            this.tweens.add({
+                delay: 1500,
+                targets: par2,
+                alpha: { from: 0, to: 1 },
+                ease: 'Sine.InOut',
+                duration: 1000,
+            });
+            this.tweens.add({
+                delay: 3000,
+                targets: par3,
+                alpha: { from: 0, to: 1 },
+                ease: 'Sine.InOut',
+                duration: 1000,
+            });
+            this.hoverInteract(par3);
+            this.add.rectangle(675, 500, 50, 100, 0xffffff);
+            this.add.text(625, 450, "placeholder for walking sprite animation",{align:"center",color:"#000000",wordWrap:{width:50}});
+
+            // note: it is possible for people to skip the cutscene because of this, might change in the future
+            par3.on("pointerdown",()=>{
+                this.time.delayedCall(1000, () => this.scene.start('gameplay'));
+            })
         }
     }
     class Transition2 extends Phaser.Scene
@@ -74,9 +147,8 @@
         {
             
 
-            const text = this.add.text(400, 300, 'Transition 2 \n 20 years later', { align: 'center' }, 0xFF69B4);
+            const text = this.add.text(400, 300, '20 years later', { align: 'center' }, 0xFF69B4);
             text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
-
             text.setOrigin(0.5, 0.5);
             text.setResolution(window.devicePixelRatio);
             text.setFontFamily('Arial');
@@ -84,6 +156,16 @@
             text.setFontSize(100);
             text.preFX.setPadding(32);
             const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+
+            const text1 = this.add.text(400, 400, 'click to continue', { align: 'center' }, 0xFF69B4);
+            text1.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+            text1.setOrigin(0.5, 0.5);
+            text1.setResolution(window.devicePixelRatio);
+            text1.setFontFamily('Arial');
+            text1.setFontStyle('bold');
+            text1.setFontSize(20);
+            text1.preFX.setPadding(32);
+            const fx1 = text1.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
             this.input.on('pointerdown', () => {
                 this.cameras.main.fade(1000, 0,0,0);
                 this.time.delayedCall(1000, () => this.scene.start('gameplay', this.data));
@@ -104,7 +186,7 @@
         {
             
 
-            const text = this.add.text(400, 300, 'Transition 3 \n 40 years later', { align: 'center' }, 0xFF69B4);
+            const text = this.add.text(400, 300, '40 years later', { align: 'center' }, 0xFF69B4);
             text.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
 
             text.setOrigin(0.5, 0.5);
@@ -115,6 +197,16 @@
             text.preFX.setPadding(32);
             const fx = text.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
 
+            const text1 = this.add.text(400, 400, 'click to continue', { align: 'center' }, 0xFF69B4);
+            text1.setTint(0xFF69B4, 0xFFC0CB, 0x9F2B68, 0xE30B5C);
+            text1.setOrigin(0.5, 0.5);
+            text1.setResolution(window.devicePixelRatio);
+            text1.setFontFamily('Arial');
+            text1.setFontStyle('bold');
+            text1.setFontSize(20);
+            text1.preFX.setPadding(32);
+            const fx1 = text1.preFX.addShadow(0, 0, 0.06, 0.75, 0x000000, 4, 0.8);
+            
             this.input.on('pointerdown', () => {
                 this.cameras.main.fade(1000, 0,0,0);
                 this.time.delayedCall(1000, () => this.scene.start('gameplay', this.data));
